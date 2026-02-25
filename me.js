5a2e8860e2f31c7904d8d5fc21b5a2185777ca90
alexag2219 <alexat2219@gmail.com>
2026-02-25 11:04:49 -0600

    Add files via upload

---
+++ me.js
@@ -0,0 +1,14 @@
+//Alexa Gomez ALL about me 2/25/26//
+
+//variable declarations//
+let myName = "Alexa";
+let favColor = "Teal";
+let favHobby = "Bedazzeling";
+let favFood = "Flautas";
+
+//display information//
+
+console.log("My name is:", myName);
+console.log("My favorite color is:", favColor);
+console.log("My favorite hobby is:",favHobby);
+console.log("My favorite food is:", favFood);
